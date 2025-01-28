7cc7f75151a5e10758e4a43e59571c264253c6c8
Andrey Arama <zekmaro@yandex.ru>
2025-01-28 17:53:23 +0100

    working on wordpress

--- containers/wordpress/wordpress_setup.sh
+++ containers/wordpress/wordpress_setup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-  echo "WordPress not found. Installing WordPress"
+  echo "Wordpress configuration file not found. Setting it up."
 
-  mkdir -p /var/www/html
-
-  curl -o wordpress.tar.gz https://wordpress.org/latest.tar.gz \
-    && tar -xvzf wordpress.tar.gz -C /var/www/html \
-    && rm wordpress.tar.gz
+  wp config create \
+    --dbname=${WORDPRESS_DB_NAME} \
+    --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASSWORD} \
+    --dbhost=${WORDPRESS_DB_HOST} \
+    --skip-check
   
-  echo "WordPress installed."
-else
-  echo "WordPress already installed."
+  echo "WordPress configured."
 fi
 
+if [ ! -d /var/www/html/wp-content ]; then
+  echo "WordPress not found. Installing it."
+
+  wp core download --path=/var/www/html --allow-root
+  wp core install \
+    --url=${WORDPRESS_URL} \
+    --title=${WORDPRESS_TITLE} \
+    --admin_user=${WORDPRESS_ADMIN_USER} \
+    --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+    --admin_email=${WORDPRESS_ADMIN_EMAIL} \
+    --skip-email \
+    --allow-root
+
+  echo "WordPress installed."
+fi
+
+chmod -R 755 /var/www/html
+exec php-fpm
