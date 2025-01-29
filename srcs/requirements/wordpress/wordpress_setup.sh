96cabd99c865d7ae160a4eda791ba87fccbcc0c4
vboxuser <vboxuser@inception.myguest.virtualbox.org>
2025-01-29 16:47:28 +0100

    its not working. its ok

--- srcs/requirements/wordpress/wordpress_setup.sh
+++ srcs/requirements/wordpress/wordpress_setup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-echo "Waiting for MariaDB to be ready..."
+log "Waiting for MariaDB to be ready..."
+
+max_retries=30
+retry_delay=5
+retries=0
+
 while ! mysqladmin ping -h"${WORDPRESS_DB_HOST}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" --silent; do
-  sleep 1
+  sleep $retry_delay
+  retries=$((retries+1))
+  if [ ! $retries -ge $max_retries ]; then
+    log "db failed"
+    exit 1
+  fi
 done
-echo "MariaDB is ready!"
+
+log "MariaDB is ready!"
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-  echo "Wordpress configuration file not found. Setting it up."
+  log "Wordpress configuration file not found. Setting it up."
 
   wp config create \
     --dbname=${WORDPRESS_DB_NAME} \
@@ -16,13 +27,12 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     --dbhost=${WORDPRESS_DB_HOST} \
     --skip-check
   
-  echo "WordPress configured."
+  log "WordPress configured."
 fi
 
 if [ ! -d /var/www/html/wp-content ]; then
-  echo "WordPress not found. Installing it. Setting up admin user."
+  log "WordPress not found. Installing it. Setting up admin user."
 
-  wp core download --path=/var/www/html --allow-root
   wp core install \
     --url=${WORDPRESS_URL} \
     --title=${WORDPRESS_TITLE} \
@@ -30,9 +40,8 @@ if [ ! -d /var/www/html/wp-content ]; then
     --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
     --admin_email=${WORDPRESS_ADMIN_EMAIL} \
     --skip-email \
-    --allow-root
 
-  echo "WordPress installed."
+  log "WordPress installed."
 fi
 
 chown -R www-data:www-data /var/www/html
